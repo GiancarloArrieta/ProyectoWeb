5e625863236cc6a1a98d92778a2f01f46544ef3c
Antonio Madriz <123045553@upq.edu.mx>
2025-10-29 18:27:04 -0500

    Cambios mínimos en Titulos

--- resources/views/interfazAdministrador.blade.php
+++ resources/views/interfazAdministrador.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel de Jefe de Soporte - Dulces Ricos</title>
+    <title>Panel de Jefe de Soporte</title>
 </head>
 <body>
     
